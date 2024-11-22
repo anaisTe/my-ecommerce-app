48e48c169983d768669c1e6171ec4c863f438039
anaisTe <anais.tejada@sinapsiscorp.com>
2024-11-22 18:23:23 -0500

    fix: layout build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/globals.css';
 import { Footer } from "./components/Footer";
 import { NavBar } from './components/NavBar';
+import { Suspense } from 'react';
 
 
 // src/app/layout.tsx
@@ -13,7 +14,9 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className="flex flex-col h-full">
         <NavBar />
-        <main>{children}</main>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <main>{children}</main>
+        </Suspense>
         <div className="mt-auto w-full">
           <Footer />
         </div>
